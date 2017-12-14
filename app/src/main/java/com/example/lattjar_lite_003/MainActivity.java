9ef3aaaff59adbd6b0d27b51904c24bfbe8016e2
Fabricio Cuenca Moldez <fcz15002@student.mdh.se>
2017-12-14 11:35:42 +0100

    Har fungerande fullskärm med korrekt roterad bild. Gjorde ändringar i onResume vilka förhindrar crashar när man roterar skärmen.

--- app/src/main/java/com/example/lattjar_lite_003/MainActivity.java
+++ app/src/main/java/com/example/lattjar_lite_003/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.lattjar_lite_003;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -12,6 +14,7 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 import org.opencv.dnn.Importer;
 import org.opencv.imgproc.Imgproc;
 
@@ -46,6 +49,11 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 
     @Override
@@ -60,7 +68,13 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     public void onResume()
     {
         super.onResume();
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+        if (!OpenCVLoader.initDebug()) {
+            //Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            //Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
     }
 
     public void onDestroy() {
@@ -76,17 +90,12 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
         Mat rgba = inputFrame.rgba();
-        //Mat converted = new Mat();
-        //Mat frame = new Mat();
-        //Mat grayframe = new Mat();
-        Mat edges = new Mat();
+        Mat gray = new Mat();
 
-        //rgba.convertTo(converted, CvType.CV_8U);
+        Imgproc.cvtColor(rgba, gray, Imgproc.COLOR_BGR2GRAY);
 
-        //Imgproc.cvtColor(converted, BGRframe, Imgproc.COLOR_RGBA2BGR);
-        Imgproc.cvtColor(rgba, edges, Imgproc.COLOR_BGR2GRAY);
-        //Imgproc.Canny(rgba, rgba, 30, 60);
-        return edges;
+        return gray;
     }
 }
