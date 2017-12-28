33e6ae62b38fb392a6a9601877abc0932fbff448
SwedFred <fredrik.areblad@gmail.com>
2017-12-28 11:16:53 +0100

    kameran har fått en egen klass

--- app/src/main/java/com/example/lattjar_lite_003/CameraGuy.java
+++ app/src/main/java/com/example/lattjar_lite_003/CameraGuy.java
@@ -1,12 +1,15 @@
 package com.example.lattjar_lite_003;
 
+import android.media.Image;
 import android.provider.ContactsContract;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.media.session.IMediaControllerCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -22,6 +25,7 @@ import org.opencv.imgproc.Imgproc;
 import java.util.List;
 
 import static org.opencv.core.CvType.CV_8UC;
+import static org.opencv.core.CvType.CV_8UC4;
 
 public class CameraGuy extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -59,6 +63,7 @@ public class CameraGuy extends AppCompatActivity implements CameraBridgeViewBase
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_camera_guy);
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+        //mOpenCvCameraView.setMaxFrameSize(200,200);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
     }
@@ -91,14 +96,28 @@ public class CameraGuy extends AppCompatActivity implements CameraBridgeViewBase
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
-        Mat knas = inputFrame.rgba();
-        Mat hehe = inputFrame.rgba();
-        Imgproc proc = new Imgproc();
-        Imgproc.threshold(knas, hehe, 100, 255, Imgproc.THRESH_BINARY);
+        //Imgproc proc = new Imgproc();
 
+        Mat rgb1 = inputFrame.rgba();
+        //Mat rgb2 = inputFrame.rgba();
+        Mat rgb2 = rgb1.clone();
 
-        return hehe;
-                //inputFrame.rgba();
+        Mat gray1 = inputFrame.gray();
+        Mat gray2 = gray1.clone();
+
+        //Testning: Vad kan jag köra och vad kan jag inte köra från denna miljö?
+        //Funkar:
+        //Imgproc.threshold(gray1, gray2, 100, 255, Imgproc.THRESH_BINARY);
+
+        Mat kek = Imgcodecs.imread(ResourcesCompat.getDrawable(getResources(), R.drawable.img, null).toString());
+
+        //Core.transpose(kek, bur);
+
+        //Core.flip(rgb2, rgb1, -1);
+        //Funkar inte:
+        //Core.completeSymm(rgb2, false);
+
+        return rgb1;
     }
 
 }
