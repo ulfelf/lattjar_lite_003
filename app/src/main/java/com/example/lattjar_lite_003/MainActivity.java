8f88e65e10e2a7b25f0d63a7121578f147cf2856
SwedFred <fredrik.areblad@gmail.com>
2018-01-09 14:01:42 +0100

    commit and push push push

--- app/src/main/java/com/example/lattjar_lite_003/MainActivity.java
+++ app/src/main/java/com/example/lattjar_lite_003/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.lattjar_lite_003;
 
 import android.content.Intent;
 import android.graphics.PixelFormat;
+import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.BaseInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
 import com.gigamole.infinitecycleviewpager.HorizontalInfiniteCycleViewPager;
@@ -24,18 +31,35 @@ import org.opencv.core.Mat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<Integer> shapeList = new ArrayList();
-    List<Integer> instrumentList = new ArrayList();
+    /*List<Integer> shapeList = new ArrayList();
+    List<Integer> instrumentList = new ArrayList();*/
+    static ImageView phone;
+    static int counter = 0;
+    static int viewLocation[] = {0,0};
+    static ArrayList<ImageView> dots = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_instruments);
+        setContentView(R.layout.tutorial_howtoplay);
 
-        populateShapes();
+
+        phone = findViewById(R.id.img_phone);
+        dots.add((ImageView)findViewById(R.id.img_blackdot));
+        dots.add((ImageView)findViewById(R.id.img_reddot));
+        dots.add((ImageView)findViewById(R.id.img_blackdot2));
+        dots.add((ImageView)findViewById(R.id.img_bluedot));
+        dots.add((ImageView)findViewById(R.id.img_greendot2));
+        dots.add((ImageView)findViewById(R.id.img_greendot));
+
+        handler.post(r);
+
+        /*populateShapes();
         populateInstruments();
 
         VerticalInfiniteCycleViewPager firstPager = (VerticalInfiniteCycleViewPager) findViewById(R.id.first_cycler);
@@ -44,10 +68,10 @@ public class MainActivity extends AppCompatActivity {
 
         VerticalInfiniteCycleViewPager secondPager = (VerticalInfiniteCycleViewPager) findViewById(R.id.second_cycler);
         CycleAdapter secondCycleAdapter = new CycleAdapter(instrumentList, getBaseContext());
-        secondPager.setAdapter(secondCycleAdapter);
+        secondPager.setAdapter(secondCycleAdapter);*/
 
     }
-    private void populateInstruments() {
+    /*private void populateInstruments() {
         instrumentList.add(R.drawable.trumpet);
         instrumentList.add(R.drawable.drum);
         instrumentList.add(R.drawable.piano);
@@ -59,7 +83,28 @@ public class MainActivity extends AppCompatActivity {
         shapeList.add(R.drawable.circle_green);
         shapeList.add(R.drawable.circle_red);
         shapeList.add(R.drawable.circle_black);
-    }
+    }*/
+
+
+
+    final Handler handler = new Handler();
+    final Runnable r = new Runnable()
+    {
+        public void run()
+        {
+         dots.get(counter % 6).getLocationInWindow(viewLocation);
+         phone.setX(viewLocation[0] - 60);
+         phone.setY(viewLocation[1] - 160);
+         counter++;
+         //Spela upp ett ljud här, följ en specifik ordning: svart, röd, svart, blå, grön, grön
+
+         handler.postDelayed(r, 1000);
+            //int nextLocation [] = { phoneLocation[0]+1, (phoneLocation[1]-72)+1 }; //Av någon anledning måste man ta -72 för att den ska flyttas korrekt. Detta gjorde att jag gjorde designvalet att bara flytta telefonjäveln manuellt.
+            /*phone.setX(viewLocation[0] - 60); Bra centrerade värden om man utgår från blackdot2s position i landskapsläge
+            phone.setY(viewLocation[1] - 160);*/
+
+        }
+    };
 
 
 
